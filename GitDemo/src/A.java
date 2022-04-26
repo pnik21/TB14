60595fbce2dac2b3228c4d662e1512da8a7fe756
sir <sir@DESKTOP-KSQ6RIJ>
2022-04-26 12:34:42 +0530

    by prasad26

--- GitDemo/src/A.java
+++ GitDemo/src/A.java
@@ -1,12 +1,26 @@
 
 public class A
 {
+	
+	
+	
+	public void m1()
+	{
+		System.out.println("m1 go git");
+	}
+	
 	public static void main(String[] args) 
 	
 	{
 	
 		System.out.println("Hello A");
 		System.out.println("Hello Cjc");
-	}
 	
+	
+	
+	
+	A a=new A();
+	a.m1();
+	
+	}
 }
